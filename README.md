3eed0be67c063be9c725ab6918ce8480d407ac4c
Daniel Li <lida_mail@163.com>
2022-11-17 12:12:52 +0800

    init version for BtSppUart, and it works fine.

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # BtSppUart
-BtSppUart for BetaFlight Configurator
+
+BtSppUart is try to connect BetaFlight Configurator remotely though bluetooth virtual com port.
+
+# Solution
+
+## Hardware
+
+| Supported Targets | ESP32 | ESP32-C2 | ESP32-C3 | ESP32-H2 | ESP32-S3 |
+| :---------------: | :---: | :------: | :------: | :------: | :------: |
+| TBD | Tested | TBD | TBD | TBD | TBD |
+
+## Software 
+
+ESP-IDF GATT SERVER SPP Example is based.
+>[esp-idf-v5.0](https://github.com/espressif/esp-idf/tree/release/v5.0) \examples\bluetooth\bluedroid\classic_bt\ [bt_spp_acceptor](https://github.com/espressif/esp-idf/tree/release/v5.0/examples/bluetooth/bluedroid/classic_bt/bt_spp_acceptor)
+
+# How to Dev & Test
+
+## Step 1: create a working dir
+
+> $ mkdir test
+> 
+> $ cd test
+
+## Step 2: clone ESP IDF v5.0
+
+> $ git clone git@github.com:espressif/esp-idf.git
+> 
+> $ cd esp-idf
+> 
+> $ git checkout release/v5.0
+
+## Step 3: setup dev environment
+
+> $ ./install.sh
+
+## Step 4: clone BleSppUart
+
+> $ cd ..
+> 
+> $ git clone git@github.com:lida2003/BtSppUart.git
+
+## Step 5: enter dev environment
+
+> $ cd esp-idf
+> 
+> $ . ./export.sh
+
+## Step 6: build BleSppUart
+
+> $ cd BtSppUart
+> 
+> $ idf.py set-target ESP32
+> 
+> $ idf.py build
+
+*Note: try idf.py set-target target, if you have other module or dev boards.*
+
+## Step 7: update firmware
+
+> $ idf.py -p /dev/ttyUSB0 flash monitor
+
+*Note: change ttyUSBx, x=serial port of the ttl module.*
+
+# Summary
---
+++ main/main.c
@@ -0,0 +1,339 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_bt_api.h"
+#include "esp_bt_device.h"
+#include "esp_spp_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_system.h"
+#include "time.h"
+#include "sys/time.h"
+#include "sdkconfig.h"
+
+
+#define SPP_TAG            "BtSppUart"
+#define SPP_SERVER_NAME     SPP_TAG
+#define EXAMPLE_DEVICE_NAME SPP_TAG
+
+
+#define TTL_UART_NUM        (UART_NUM_1)
+#define TTL_TEST_TXD        (GPIO_NUM_4)
+#define TTL_TEST_RXD        (GPIO_NUM_5)
+#define TTL_BUF_SIZE        (1024)
+
+
+static struct timeval time_new, time_old;
+static long data_num = 0;
+static const esp_spp_mode_t esp_spp_mode = ESP_SPP_MODE_CB;
+static const esp_spp_sec_t sec_mask = ESP_SPP_SEC_AUTHENTICATE;
+static const esp_spp_role_t role_slave = ESP_SPP_ROLE_SLAVE;
+static uint32_t esp_ssp_handle = 0;
+static bool bt_is_connected = false;
+QueueHandle_t ttl_uart_queue = NULL;
+
+void ttl_uart_task(void *pvParameters)
+{
+    uart_event_t event;
+
+    for (;;) {
+        //Waiting for UART event.
+        if (xQueueReceive(ttl_uart_queue, (void * )&event, (TickType_t)portMAX_DELAY)) {
+            switch (event.type) {
+            //Event of UART receving data
+            case UART_DATA:
+                if ((event.size)&&(bt_is_connected)) {
+                    uint8_t * temp = NULL;
+
+                    temp = (uint8_t *)malloc(sizeof(uint8_t)*event.size);
+                    if(temp == NULL){
+                        ESP_LOGE(SPP_TAG, "%s malloc.1 failed\n", __func__);
+                        break;
+                    }
+                    memset(temp,0x0,event.size);
+                    uart_read_bytes(TTL_UART_NUM, temp, event.size, portMAX_DELAY);
+					
+					ESP_LOGI(SPP_TAG, "ttl read %d Bytes\n", event.size);
+					if (event.size < 128) {
+						esp_log_buffer_hex(SPP_TAG, temp, event.size);
+					}
+
+					// To do send to BT SPP
+					if (esp_ssp_handle){
+						esp_spp_write(esp_ssp_handle, event.size, temp);
+					}
+                    //esp_ble_gatts_send_indicate(spp_gatts_if, spp_conn_id, spp_handle_table[SPP_IDX_SPP_DATA_NTY_VAL],event.size, temp, false);
+                    free(temp);
+                }
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+void ttl_uart_init(void)
+{
+	uart_config_t uart_config = {
+		.baud_rate = 115200,
+		.data_bits = UART_DATA_8_BITS,
+		.parity = UART_PARITY_DISABLE,
+		.stop_bits = UART_STOP_BITS_1,
+		.flow_ctrl = UART_HW_FLOWCTRL_RTS,
+		.rx_flow_ctrl_thresh = 122,
+		.source_clk = UART_SCLK_DEFAULT,
+	};
+
+    //Install UART driver, and get the queue.
+    uart_driver_install(TTL_UART_NUM, TTL_BUF_SIZE * 2, 8192, 10, &ttl_uart_queue, 0);
+    //Set UART parameters
+    uart_param_config(TTL_UART_NUM, &uart_config);
+    //Set UART pins
+    uart_set_pin(TTL_UART_NUM, TTL_TEST_TXD, TTL_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    xTaskCreate(ttl_uart_task, "ttl_uart_task", 2048, (void*)TTL_UART_NUM, 8, NULL);
+}
+
+static char *bda2str(uint8_t * bda, char *str, size_t size)
+{
+    if (bda == NULL || str == NULL || size < 18) {
+        return NULL;
+    }
+
+    uint8_t *p = bda;
+    sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
+            p[0], p[1], p[2], p[3], p[4], p[5]);
+    return str;
+}
+
+static void print_speed(void)
+{
+    float time_old_s = time_old.tv_sec + time_old.tv_usec / 1000000.0;
+    float time_new_s = time_new.tv_sec + time_new.tv_usec / 1000000.0;
+    float time_interval = time_new_s - time_old_s;
+    float speed = data_num * 8 / time_interval / 1000.0;
+    ESP_LOGI(SPP_TAG, "speed(%fs ~ %fs): %f kbit/s" , time_old_s, time_new_s, speed);
+    data_num = 0;
+    time_old.tv_sec = time_new.tv_sec;
+    time_old.tv_usec = time_new.tv_usec;
+}
+
+static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+{
+    char bda_str[18] = {0};
+
+    switch (event) {
+    case ESP_SPP_INIT_EVT:
+        if (param->init.status == ESP_SPP_SUCCESS) {
+            ESP_LOGI(SPP_TAG, "ESP_SPP_INIT_EVT");
+            esp_spp_start_srv(sec_mask, role_slave, 0, SPP_SERVER_NAME);
+        } else {
+            ESP_LOGE(SPP_TAG, "ESP_SPP_INIT_EVT status:%d", param->init.status);
+        }
+        break;
+    case ESP_SPP_DISCOVERY_COMP_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_DISCOVERY_COMP_EVT");
+        break;
+    case ESP_SPP_OPEN_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_OPEN_EVT");
+        break;
+    case ESP_SPP_CLOSE_EVT:
+		bt_is_connected = false;
+        ESP_LOGI(SPP_TAG, "ESP_SPP_CLOSE_EVT status:%d handle:%d close_by_remote:%d", param->close.status,
+                 param->close.handle, param->close.async);
+        break;
+    case ESP_SPP_START_EVT:
+        if (param->start.status == ESP_SPP_SUCCESS) {
+            ESP_LOGI(SPP_TAG, "ESP_SPP_START_EVT handle:%d sec_id:%d scn:%d", param->start.handle, param->start.sec_id,
+                     param->start.scn);
+            esp_bt_dev_set_device_name(EXAMPLE_DEVICE_NAME);
+            esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+        } else {
+            ESP_LOGE(SPP_TAG, "ESP_SPP_START_EVT status:%d", param->start.status);
+        }
+        break;
+    case ESP_SPP_CL_INIT_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_CL_INIT_EVT");
+        break;
+    case ESP_SPP_DATA_IND_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_DATA_IND_EVT len:%d handle:%d",
+                 param->data_ind.len, param->data_ind.handle);
+        if (param->data_ind.len < 128) {
+            esp_log_buffer_hex(SPP_TAG, param->data_ind.data, param->data_ind.len);
+        }else{
+	        gettimeofday(&time_new, NULL);
+	        data_num += param->data_ind.len;
+	        if (time_new.tv_sec - time_old.tv_sec >= 3) {
+	            print_speed();
+	        }
+        }
+		uart_write_bytes(TTL_UART_NUM, param->data_ind.data, param->data_ind.len);
+        break;
+    case ESP_SPP_CONG_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_CONG_EVT");
+        break;
+    case ESP_SPP_WRITE_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_WRITE_EVT");
+        break;
+    case ESP_SPP_SRV_OPEN_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_OPEN_EVT status:%d handle:%d, rem_bda:[%s]", param->srv_open.status,
+                 param->srv_open.handle, bda2str(param->srv_open.rem_bda, bda_str, sizeof(bda_str)));
+		bt_is_connected = true;
+		esp_ssp_handle = param->srv_open.handle;
+		gettimeofday(&time_old, NULL);
+        break;
+    case ESP_SPP_SRV_STOP_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_STOP_EVT");
+		esp_ssp_handle = 0;
+        break;
+    case ESP_SPP_UNINIT_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_SPP_UNINIT_EVT");
+        break;
+    default:
+        break;
+    }
+}
+
+void esp_bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    char bda_str[18] = {0};
+
+    switch (event) {
+    case ESP_BT_GAP_AUTH_CMPL_EVT:{
+        if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGI(SPP_TAG, "authentication success: %s bda:[%s]", param->auth_cmpl.device_name,
+                     bda2str(param->auth_cmpl.bda, bda_str, sizeof(bda_str)));
+        } else {
+            ESP_LOGE(SPP_TAG, "authentication failed, status:%d", param->auth_cmpl.stat);
+        }
+        break;
+    }
+    case ESP_BT_GAP_PIN_REQ_EVT:{
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_PIN_REQ_EVT min_16_digit:%d", param->pin_req.min_16_digit);
+        if (param->pin_req.min_16_digit) {
+            ESP_LOGI(SPP_TAG, "Input pin code: 0000 0000 0000 0000");
+            esp_bt_pin_code_t pin_code = {0};
+            esp_bt_gap_pin_reply(param->pin_req.bda, true, 16, pin_code);
+        } else {
+            ESP_LOGI(SPP_TAG, "Input pin code: 1234");
+            esp_bt_pin_code_t pin_code;
+            pin_code[0] = '1';
+            pin_code[1] = '2';
+            pin_code[2] = '3';
+            pin_code[3] = '4';
+            esp_bt_gap_pin_reply(param->pin_req.bda, true, 4, pin_code);
+        }
+        break;
+    }
+
+#if (CONFIG_BT_SSP_ENABLED == true)
+    case ESP_BT_GAP_CFM_REQ_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_CFM_REQ_EVT Please compare the numeric value: %d", param->cfm_req.num_val);
+        esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
+        break;
+    case ESP_BT_GAP_KEY_NOTIF_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_NOTIF_EVT passkey:%d", param->key_notif.passkey);
+        break;
+    case ESP_BT_GAP_KEY_REQ_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_REQ_EVT Please enter passkey!");
+        break;
+#endif
+
+    case ESP_BT_GAP_MODE_CHG_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_MODE_CHG_EVT mode:%d bda:[%s]", param->mode_chg.mode,
+                 bda2str(param->mode_chg.bda, bda_str, sizeof(bda_str)));
+        break;
+
+    default: {
+        ESP_LOGI(SPP_TAG, "event: %d", event);
+        break;
+    }
+    }
+    return;
+}
+
+void app_main(void)
+{
+    char bda_str[18] = {0};
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+
+	ttl_uart_init();
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    if ((ret = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s initialize controller failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s enable controller failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_bluedroid_init()) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s initialize bluedroid failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_bluedroid_enable()) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s enable bluedroid failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_bt_gap_register_callback(esp_bt_gap_cb)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s gap register failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_spp_register_callback(esp_spp_cb)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s spp register failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_spp_init(esp_spp_mode)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s spp init failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+#if (CONFIG_BT_SSP_ENABLED == true)
+    /* Set default parameters for Secure Simple Pairing */
+    esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
+    esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
+    esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
+#endif
+
+    /*
+     * Set default parameters for Legacy Pairing
+     * Use variable pin, input pin code when pairing
+     */
+    esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_VARIABLE;
+    esp_bt_pin_code_t pin_code;
+    esp_bt_gap_set_pin(pin_type, 0, pin_code);
+
+    ESP_LOGI(SPP_TAG, "Own address:[%s]", bda2str((uint8_t *)esp_bt_dev_get_address(), bda_str, sizeof(bda_str)));
+}
